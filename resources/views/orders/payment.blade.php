d29832f42b39a0ec968c3348cdb0cf652a7d4970
unknown <alfredomarenco@boletea.com>
2021-11-04 23:05:55 -0600

    Creamos cambios

--- resources/views/orders/payment.blade.php
+++ resources/views/orders/payment.blade.php
@@ -33,111 +33,124 @@
         $preference->items = $products;
         $preference->save();
     @endphp
-    <div class="container py-8">
-        <div class="bg-white rounded-lg shadow-lg px-6 py-4 mb-6">
-            <p class="text-trueGray-700 uppercase">
-                <span class="font-semibold">Número de orden:</span> Orden-{{ $order->id }}
-            </p>
-        </div>
-        <div class="bg-white rounded-lg shadow-lg p-6 mb-6">
-            <div class="grid grid-cols-2 gap-6">
-                <div>
-                    <p class="text-lg font-semibold uppercase">
-                        Envío
-                    </p>
-                    @if ($order->shipping_type == 1)
-                        <p class="text-sm">Los productos deben ser recogidos en tienda</p>
-                        <p class="text-sm">Calle falsa 123</p>
-                    @else
-                        <p class="text-sm">Los productos serán enviados a</p>
-                        <p class="text-sm">{{ $order->address }}</p>
-                        <p>{{ $order->department->name }} - {{ $order->city->name }} -
-                            {{ $order->district->name }}</p>
-                    @endif
-                </div>
-                <div>
-                    <p class="text-lg font-semibold uppercase">
-                        Datos de contacto
-                    </p>
-                    <p class="text-sm">
-                        Persona que recibirá el producto: {{ $order->contact }}
-                    </p>
-                    <p class="text-sm">
-                        Telefono de contacto: {{ $order->phone }}
-                    </p>
+    <div class="grid grid-cols-5 gap-6 container py-8">
+        <div class="col-span-3">
+            <div class="bg-white rounded-lg shadow-lg px-6 py-4 mb-6">
+                <p class="text-trueGray-700 uppercase">
+                    <span class="font-semibold">Número de orden:</span> Orden-{{ $order->id }}
+                </p>
+            </div>
+            <div class="bg-white rounded-lg shadow-lg p-6 mb-6">
+                <div class="grid grid-cols-2 gap-6">
+                    <div>
+                        <p class="text-lg font-semibold uppercase">
+                            Envío
+                        </p>
+                        @if ($order->shipping_type == 1)
+                            <p class="text-sm">Los productos deben ser recogidos en tienda</p>
+                            <p class="text-sm">Calle falsa 123</p>
+                        @else
+                            <p class="text-sm">Los productos serán enviados a</p>
+                            <p class="text-sm">{{ $order->address }}</p>
+                            <p>{{ $order->department->name }} - {{ $order->city->name }} -
+                                {{ $order->district->name }}</p>
+                        @endif
+                    </div>
+                    <div>
+                        <p class="text-lg font-semibold uppercase">
+                            Datos de contacto
+                        </p>
+                        <p class="text-sm">
+                            Persona que recibirá el producto: {{ $order->contact }}
+                        </p>
+                        <p class="text-sm">
+                            Telefono de contacto: {{ $order->phone }}
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="bg-white rounded-lg shadow-lg p-6 text-trueGray-700 mb-6">
-            <p class="text-xl font-semibold mb-4">Resumen</p>
+            <div class="bg-white rounded-lg shadow-lg p-6 text-trueGray-700 mb-6">
+                <p class="text-xl font-semibold mb-4">Resumen</p>
 
-            <table class="table-auto w-full">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Precio</th>
-                        <th>Cant</th>
-                        <th>Total</th>
-                    </tr>
-                </thead>
-
-                <tbody class="divide-y divide-gray-200">
-                    @foreach ($items as $product)
+                <table class="table-auto w-full">
+                    <thead>
                         <tr>
-                            <td>
-                                <div class="flex">
-                                    <img src="{{ $product->options->image }}" alt=""
-                                        class="h-15 w-20 object-cover mr-4">
-                                    <article>
-                                        <h1 class="font-bold">{{ $product->name }}</h1>
-                                        <div class="flex text-xs">
-                                            @isset($product->options->color)
-                                                Color: {{ __($product->options->color) }}
-                                            @endisset
-
-                                            @isset($product->options->size)
-                                                Talla: {{ __($product->options->size) }}
-                                            @endisset
-                                        </div>
-                                    </article>
-                                </div>
-                            </td>
-                            <td class="text-center">
-                                {{ $product->price }} USD
-                            </td>
-                            <td class="text-center">
-                                {{ $product->qty }}
-                            </td>
-                            <td class="text-center">
-                                {{ $product->price * $product->qty }}
-                            </td>
+                            <th></th>
+                            <th>Precio</th>
+                            <th>Cant</th>
+                            <th>Total</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+
+                    <tbody class="divide-y divide-gray-200">
+                        @foreach ($items as $product)
+                            <tr>
+                                <td>
+                                    <div class="flex">
+                                        <img src="{{ $product->options->image }}" alt=""
+                                            class="h-15 w-20 object-cover mr-4">
+                                        <article>
+                                            <h1 class="font-bold">{{ $product->name }}</h1>
+                                            <div class="flex text-xs">
+                                                @isset($product->options->color)
+                                                    Color: {{ __($product->options->color) }}
+                                                @endisset
+
+                                                @isset($product->options->size)
+                                                    Talla: {{ __($product->options->size) }}
+                                                @endisset
+                                            </div>
+                                        </article>
+                                    </div>
+                                </td>
+                                <td class="text-center">
+                                    {{ $product->price }} USD
+                                </td>
+                                <td class="text-center">
+                                    {{ $product->qty }}
+                                </td>
+                                <td class="text-center">
+                                    {{ $product->price * $product->qty }}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+
         </div>
 
-        <div class="bg-white rounded-lg shadow-lg p-6 flex items-center justify-between">
-            <img class="h-12"
-                src="https://lh3.googleusercontent.com/proxy/KhLaArJk9MYZH5rPhOFBo5qWumWlzd9a1rj4Lcx1tD9U6YnqmC3CVw_QyUFzfW09Ay0PYOauHgcPFwVczQLpdNGNP37IGntd3PHu5hHweYxeswZeoeAHWyN8UW01JQZ-9xHZRMBhBA4dktEdqS3e0g7Hf2uiWB_UgQ"
-                alt="">
-            <div class="text-trueGray-700">
-                <p class="font-semibold text-sm">
-                    Subtotal: {{ $order->total - $order->shipping_cost }} USD
-                </p>
-                <p class="font-semibold text-sm">
-                    Envio: {{ $order->shipping_cost }} USD
-                </p>
-                <p class="font-semibold uppercase text-lg">
-                    Total: {{ $order->total }} USD
-                </p>
-                <div class="cho-container">
-
+        <div class="col-span-2">
+            <div class="bg-white rounded-lg shadow-lg p-6">
+                <div>
+                    <div class="text-center mb-2 bg-trueGray-900 text-white">
+                        <h2 class="font-semibold text-xl">Metodos de pago</h2>
+                    </div>
+                    <div class="text-trueGray-700 text-right">
+                        <p class="font-semibold text-sm">
+                            Subtotal: {{ $order->total - $order->shipping_cost }} USD
+                        </p>
+                        <p class="font-semibold text-sm">
+                            Envio: {{ $order->shipping_cost }} USD
+                        </p>
+                        <p class="font-semibold uppercase text-lg">
+                            Total: {{ $order->total }} USD
+                        </p>
+                    </div>
+                    <div class="text-center">
+                        <div class="cho-container w-full ">
+                        </div>
+                    </div>
+                    <img class="h-10 mx-auto mt-4"
+                        src="https://lh3.googleusercontent.com/proxy/KhLaArJk9MYZH5rPhOFBo5qWumWlzd9a1rj4Lcx1tD9U6YnqmC3CVw_QyUFzfW09Ay0PYOauHgcPFwVczQLpdNGNP37IGntd3PHu5hHweYxeswZeoeAHWyN8UW01JQZ-9xHZRMBhBA4dktEdqS3e0g7Hf2uiWB_UgQ"
+                        alt="">
                 </div>
             </div>
         </div>
     </div>
+
     <script src="https://sdk.mercadopago.com/js/v2"></script>
     <script>
         // Agrega credenciales de SDK
@@ -152,7 +165,7 @@
             },
             render: {
                 container: '.cho-container', // Indica el nombre de la clase donde se mostrará el botón de pago
-                label: 'Pagar', // Cambia el texto del botón de pago (opcional)
+                label: 'Pagar con Mercado Pago', // Cambia el texto del botón de pago (opcional)
             }
         });
     </script>
